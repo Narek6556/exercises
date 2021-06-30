c2022600c1f3df98794bb78d12d26a574f4ad370
Narek6556 <aghasyan.na@gmail.com>
2021-06-30 15:48:34 +0400

    added another solution by saveing arr value to obj property name

--- unicCount.js
+++ unicCount.js
@@ -1,4 +1,4 @@
-let arr = [1, 2, 1, 1, 2, 3];
+let arr = [1, 2, 1, 1, 2, 3, 6, 7, 8, 8, 1, 6, 7];
 
 function unicCount(arr) {
     let count = 0;
@@ -15,3 +15,22 @@ function unicCount(arr) {
 
 console.log(unicCount(arr));
 
+//another solution
+
+function unicCount_2(arr) {
+    let map = {}
+    let count = 0;
+
+    for(let i = 0; i < arr.length; i++) {
+        map[arr[i]] = 0;
+    }
+    for(i = 0; i < arr.length; i++) {
+        map[arr[i]]++;
+    }
+    for (num in map) {
+        count += (map[num] * (map[num] - 1)) / 2;
+    }
+    return(count);
+}
+
+console.log(unicCount_2(arr));
